44682f26129112443cab98e6d1156c2efbacf30f
Yohey Kuwa <35585019+heykwarb@users.noreply.github.com>
2024-08-29 19:50:14 +0900

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AquaForSwiftUI",
+    platforms: [.iOS(.v17)],
+    products: [ // `products` specifies what the package provides for external use (libraries or binaries).
+        .library(
+            name: "AquaForSwiftUI",
+            targets: ["AquaForSwiftUI"]),
+    ],
+    dependencies: [],
+    targets: [ // `targets` defines the internal modules and their dependencies used for building the package.
+        // Library target
+        .target(
+            name: "AquaForSwiftUI", // Name of the target
+            dependencies: [] // Other targets or external packages this target depends on
+        ),
+        
+        // Test target
+        .testTarget(
+            name: "AquaForSwiftUITests", // Name of the test target
+            dependencies: ["AquaForSwiftUI"] // Specify the target to be tested
+        ),
+    ]
+)
